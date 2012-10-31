1688515e3556f03d467ecbbe38fd683814ff8a04
Jeroen Fiege <jeroenf@zicht.nl>
2012-10-31 16:33:59 +0000

    added composer.json, added structure for bundle
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@12822 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FrameworkExtraBundle/ZichtFrameworkExtraBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Bundle\FrameworkExtraBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ZichtFrameworkExtraBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
