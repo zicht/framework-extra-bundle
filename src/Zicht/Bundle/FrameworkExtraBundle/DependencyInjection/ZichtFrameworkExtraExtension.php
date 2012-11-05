fb06237cd871440e96eeffe3ef3c3f9f24009d91
Jeroen Fiege <jeroenf@zicht.nl>
2012-11-05 15:19:23 +0000

    registered embed_helper as a service
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@12972 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FrameworkExtraBundle/DependencyInjection/ZichtFrameworkExtraExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Bundle\FrameworkExtraBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use \Symfony\Component\HttpKernel\DependencyInjection\Extension as DIExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class ZichtFrameworkExtraExtension extends DIExtension
+{
+    /**
+     * @{inheritDoc}
+     */
+    public function load(array $config, ContainerBuilder $container) {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
