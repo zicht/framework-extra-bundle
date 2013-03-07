6d753732071901ef7e65b01ce78b10b7dd6371a1
Gerard van Helden <gerard@zicht.nl>
2013-03-07 16:50:35 +0000

    Added utility controller for admin actions.
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@17294 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FrameworkExtraBundle/Controller/CRUDController.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Bundle\FrameworkExtraBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as BaseCRUDController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Provides some basic utility functionality for admin controllers, to be supplied as an construction parameter
+ */
+class CRUDController extends BaseCRUDController
+{
+    public function showAction($id = null)
+    {
+        $obj = $this->admin->getObject($id);
+        if ($this->container->has('zicht_url.provider') && $this->get('zicht_url.provider')->supports($obj)) {
+            return $this->redirect($this->get('zicht_url.provider')->url($obj));
+        }
+        return parent::showAction($id);
+    }
+
+
+    public function editAction($id = null)
+    {
+        if ($this->get('request')->get('__bind_only')) {
+            return $this->bindAndRender('edit');
+        }
+        return parent::editAction($id);
+    }
+
+
+    public function moveUpAction($id)
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository($this->admin->getClass());
+        $result = $repo->find($id);
+        $repo->moveUp($result);
+        return $this->redirect($this->getRequest()->headers->get('referer'));
+    }
+
+
+    public function moveDownAction($id)
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository($this->admin->getClass());
+        $result = $repo->find($id);
+        $repo->moveDown($result);
+        return $this->redirect($this->getRequest()->headers->get('referer'));
+    }
+
+
+    protected function bindAndRender($action) {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        if ($action == 'edit') {
+            $id = $this->get('request')->get($this->admin->getIdParameter());
+
+            $object = $this->admin->getObject($id);
+
+            if (!$object) {
+                throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+            }
+
+            if (false === $this->admin->isGranted('EDIT', $object)) {
+                throw new AccessDeniedException();
+            }
+        } else {
+            $object = $this->admin->getNewInstance();
+
+            $this->admin->setSubject($object);
+
+            /** @var $form \Symfony\Component\Form\Form */
+            $form = $this->admin->getForm();
+            $form->setData($object);
+        }
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->get('request')->getMethod() == 'POST') {
+            $form->bind($this->get('request'));
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => $action,
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+}
