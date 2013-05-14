bdb00e3caea3408e5fab13c9edebac44e8c1e66f
Gerard van Helden <gerard@zicht.nl>
2013-05-14 14:30:34 +0000

    added file browser (cms) + documents import command
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@19697 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FrameworkExtraBundle/Doctrine/DoctrineQueryPager.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+namespace Zicht\Bundle\FrameworkExtraBundle\Doctrine;
+
+use Zicht\Bundle\FrameworkExtraBundle\Pager\Pageable;
+use Doctrine\ORM\QueryBuilder;
+
+class DoctrineQueryPager implements Pageable
+{
+    function __construct(QueryBuilder $q)
+    {
+        $this->qb = $q;
+    }
+
+
+    /**
+     * Returns the absolute total of the pageable set of elements.
+     *
+     * @return int
+     */
+    function getTotal()
+    {
+        $c = clone $this->qb;
+        $c->select('COUNT(f) c');
+        return $c->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * Sets the range that needs to be displayed on the current page
+     *
+     * @param int $start
+     * @param int $length
+     * @return int
+     */
+    function setRange($start, $length)
+    {
+        $this->qb->setFirstResult($start)->setMaxResults($length);
+    }
+}
