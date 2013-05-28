c8c7835a91ddeadb970d40eb8556dda6889f717b
Gerard van Helden <gerard@zicht.nl>
2013-05-28 12:28:39 +0000

    docs
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@20072 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/FrameworkExtraBundle/Twig/ControlStructures/WithTokenParser.php
+++ src/Zicht/Bundle/FrameworkExtraBundle/Twig/ControlStructures/WithTokenParser.php
@@ -16,47 +16,6 @@ use \Twig_NodeInterface;
  * {% with expr [as localName] [, expr2 [as localName2], [....]]  {sandboxed|merged} %}
  *     content
  * {% endwith %}
- *
- * The with construct sets the argument as the current context. If an 'as name' is defined,
- * the variable is defined as that local name. The two flags sandboxed or merged define an
- * additional behaviour, allowing to sandbox the contents of the construct from the current
- * scope (which results in having only the defined variables in the current scope), or merging
- * them with the current scope respectively. If none of the flags is defined, the current
- * context is stacked in the context as _parent. After the end of the with construct, the
- * context is restored.
- *
- * Example:
- * Assume the following context:
- * array(
- *    'foo' => array(
- *       'name' => 'Foo',
- *       'id'   => 1
- *    ),
- *    'bar' => array(
- *       'name' => 'Bar',
- *       'id'   => 2
- *    )
- * )
- *
- * {% with foo %}
- *    {{ id }}: {{ name }} {# would output: "1: Foo" #}
- * {% endwith %}
- *
- * {% with foo as baz %}
- *    {{ baz.id }}: {{ baz.name }} {# would output: "1: Foo" #}
- * {% endwith %}
- *
- * {% with foo as bar, bar as foo %}
- *    {{ bar.id }}: {{ bar.name }} {# would output: "1: Foo" #}
- *    {{ foo.id }}: {{ foo.name }} {# would output: "2: Bar" #}
- * {% endwith %}
- *
- * {% with foo merged %}
- *    {{ foo.id }}: {{ foo.name }} {# would output: "1: Foo" #}
- *    {{ id }}: {{ name }} {# would output: "1: Foo" #}
- * {% endwith %}
- *
- * etcetera.
  */
 class WithTokenParser extends \Twig_TokenParser
 {
