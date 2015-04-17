0f4c420a86ce609c5b957c72f04b5172a687dec3
Rik van der Kemp <rik@zicht.nl>
2015-04-17 10:07:52 +0000

    added a command to fix nested tree sets
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/branches/1.x@60677 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/FrameworkExtraBundle/Command/RepairNestedTreeCommand.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author Rik van der Kemp <rik@zicht.nl>
+ * @copyright Zicht Online <http://www.zicht.nl>
+ */
+
+namespace Zicht\Bundle\FrameworkExtraBundle\Command;
+
+use \Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use \Symfony\Component\Console\Input\InputArgument;
+use \Symfony\Component\Console\Input\InputInterface;
+use \Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Checks if a NestedTree set is verified (correct), if not repairs the tree
+ * using the NestedSet methods.
+ *
+ * @package Zicht\Bundle\FrameworkExtraBundle\Command
+ */
+class RepairNestedTreeCommand extends ContainerAwareCommand
+{
+    /**
+     * @{inheritDoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('zicht:repair:nested-tree')
+            ->addArgument('entity', InputArgument::REQUIRED, 'The entity to be repaired, must be of nested tree set. E.g. ZichtProjectSite:Term')
+        ;
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $doctrine = $this->getContainer()->get('doctrine');
+        $entity = $input->getArgument('entity');
+
+        $repository = $doctrine
+            ->getRepository($entity);
+        ;
+
+        if ($repository instanceof NestedTreeRepository) {
+            if ($repository->verify() !== true) {
+                $output->writeln("Errors found in tree, calling recover");
+                $repository->recover();
+                $doctrine->getManager()->flush();
+            } else {
+                $output->writeln("No issues found");
+            }
+        } else {
+            $output->writeln("Given entity is not of instance NestedTreeRepository");
+        }
+    }
+}
