85db3a5443dd71a41045c2c2ea23c7f750ed6f8e
Gerard van Helden <gerard@zicht.nl>
2015-05-27 13:30:16 +0000

    CS
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtFrameworkExtraBundle/trunk@62738 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/FrameworkExtraBundle/Command/ValidateEntityCommand.php
+++ src/Zicht/Bundle/FrameworkExtraBundle/Command/ValidateEntityCommand.php
@@ -10,8 +10,15 @@ use \Symfony\Component\Console\Input\InputOption;
 use \Symfony\Component\Console\Output\OutputInterface;
 use \Symfony\Component\Console\Input\InputInterface;
 
+/**
+ * Class ValidateEntityCommand
+ * @package Zicht\Bundle\FrameworkExtraBundle\Command
+ */
 class ValidateEntityCommand extends ContainerAwareCommand
 {
+    /**
+     * @{inheritDoc}
+     */
     protected function configure()
     {
         $this
@@ -22,16 +29,19 @@ class ValidateEntityCommand extends ContainerAwareCommand
         ;
     }
 
+    /**
+     * @{inheritDoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $groups = $input->getOption('group');
         $repo = $this->getContainer()->get('doctrine')->getRepository($input->getArgument('entity'));
 
-        foreach ($repo->findAll() as $page) {
-            $violations = $this->getContainer()->get('validator')->validate($page, $groups ? $groups : null);
+        foreach ($repo->findAll() as $entity) {
+            $violations = $this->getContainer()->get('validator')->validate($entity, $groups ? $groups : null);
 
             if (count($violations)) {
-                $output->writeln(get_class($page) . "::" . $page->getId());
+                $output->writeln(get_class($entity) . "::" . $entity->getId());
                 foreach ($violations as $error) {
                     $output->writeln(" -> {$error}");
                 }
