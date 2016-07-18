e6604c421146462fbf6703bff86b1bf364aa75a1
Muhammed Akbulut <muhammed@zicht.nl>
2016-07-18 17:29:40 +0200

    moved build time configuration attempts to compiler pass time

--- src/Zicht/Bundle/FrameworkExtraBundle/ZichtFrameworkExtraBundle.php
+++ src/Zicht/Bundle/FrameworkExtraBundle/ZichtFrameworkExtraBundle.php
@@ -8,10 +8,21 @@ namespace Zicht\Bundle\FrameworkExtraBundle;
 
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\HttpKernel\Bundle\Bundle;
+use Zicht\Bundle\FrameworkExtraBundle\DependencyInjection\Compiler\Pass;
 
 /**
  * Bundle entry point
  */
 class ZichtFrameworkExtraBundle extends Bundle
 {
+    /**
+     * @inheritdoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container); 
+        
+        $container->addCompilerPass(new Pass());
+    }
+
 }
