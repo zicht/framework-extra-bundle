5d90065c434dcbf029a3a7e5a83c406ee0b011ff
Gerard van Helden <gerard@zicht.nl>
2016-07-22 20:27:05 +0200

    inverted the logic - remove when not applicable

--- src/Zicht/Bundle/FrameworkExtraBundle/DependencyInjection/ZichtFrameworkExtraExtension.php
+++ src/Zicht/Bundle/FrameworkExtraBundle/DependencyInjection/ZichtFrameworkExtraExtension.php
@@ -100,6 +100,9 @@ class ZichtFrameworkExtraExtension extends DIExtension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
 
+        $loader->load('doctrine.xml');
+        $loader->load('imagine.xml');
+
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
