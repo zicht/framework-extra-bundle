5727fd9c711218ad3dbbcf49e046de1551c7ea0a
Jochem Klaver <jochem@zicht.nl>
2020-05-15 12:06:31 +0200

    Remove RequireJs and Uglify

--- src/Zicht/Bundle/FrameworkExtraBundle/DependencyInjection/ZichtFrameworkExtraExtension.php
+++ src/Zicht/Bundle/FrameworkExtraBundle/DependencyInjection/ZichtFrameworkExtraExtension.php
@@ -5,100 +5,17 @@
 
 namespace Zicht\Bundle\FrameworkExtraBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension as DIExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension as DIExtension;
 
 /**
  * DI extension for the bundle
  */
 class ZichtFrameworkExtraExtension extends DIExtension
 {
-    /**
-     * @param string $uglifyConfigFile
-     * @param boolean $isDebug
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     * @return void
-     *
-     * @throws \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     */
-    public function addUglifyConfiguration($uglifyConfigFile, $isDebug, ContainerBuilder $container)
-    {
-        if (!is_file($uglifyConfigFile)) {
-            throw new InvalidConfigurationException(
-                "zicht_framework_extra.uglify setting '$uglifyConfigFile' is not a file"
-            );
-        }
-
-        $container->addResource(new FileResource($uglifyConfigFile));
-
-        try {
-            $uglifyConfig = Yaml::parse(file_get_contents($uglifyConfigFile));
-        } catch (\Exception $e) {
-            throw new InvalidConfigurationException(
-                "zicht_framework_extra.uglify setting '$uglifyConfigFile' could not be read",
-                0,
-                $e
-            );
-        }
-
-        $global = new Definition(
-            'Zicht\Bundle\FrameworkExtraBundle\Twig\UglifyGlobal',
-            [
-                $uglifyConfig,
-                $isDebug,
-            ]
-        );
-
-        $global->addTag('twig.global');
-        $global->addMethodCall('setDebug', [$isDebug]);
-        $container->getDefinition('zicht_twig_extension')->addMethodCall('setGlobal', ['zicht_uglify', $global]);
-    }
-
-    /**
-     * @param string $requirejsConfigFile
-     * @param boolean $isDebug
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     * @return void
-     *
-     * @throws \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     */
-    public function addRequirejsConfiguration($requirejsConfigFile, $isDebug, ContainerBuilder $container)
-    {
-        if (!is_file($requirejsConfigFile)) {
-            throw new InvalidConfigurationException(
-                "zicht_framework_extra.requirejs setting '$requirejsConfigFile' is not a file"
-            );
-        }
-        $container->addResource(new FileResource($requirejsConfigFile));
-        try {
-            $requirejsConfig = Yaml::parse($requirejsConfigFile);
-        } catch (\Exception $e) {
-            throw new InvalidConfigurationException(
-                "zicht_framework_extra.requirejs setting '$requirejsConfigFile' could not be read",
-                0,
-                $e
-            );
-        }
-
-        $global = new Definition(
-            'Zicht\Bundle\FrameworkExtraBundle\Twig\RequirejsGlobal',
-            [
-                $requirejsConfig,
-                $isDebug,
-            ]
-        );
-
-        $global->addTag('twig.global');
-        $global->addMethodCall('setDebug', [$isDebug]);
-        $container->getDefinition('zicht_twig_extension')->addMethodCall('setGlobal', ['zicht_requirejs', $global]);
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -113,22 +30,6 @@ class ZichtFrameworkExtraExtension extends DIExtension
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        if (!empty($config['uglify'])) {
-            if (!isset($config['uglify_debug'])) {
-                $config['uglify_debug'] = $container->getParameter('kernel.debug');
-            }
-
-            $this->addUglifyConfiguration($config['uglify'], $config['uglify_debug'], $container);
-        }
-
-        if (!empty($config['requirejs'])) {
-            if (!isset($config['requirejs_debug'])) {
-                $config['requirejs_debug'] = $container->getParameter('kernel.debug');
-            }
-
-            $this->addRequirejsConfiguration($config['requirejs'], $config['requirejs_debug'], $container);
-        }
 
         if (!empty($config['embed_helper'])) {
             $container->getDefinition('zicht_embed_helper')
